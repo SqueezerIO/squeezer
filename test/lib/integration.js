0aa97d730204940aed8f76291efbab88d137d23f
Nick Chisiu <nick@squeezer.io>
2017-05-23 09:27:40 +0300

    fixed integration tests + linter

--- test/lib/integration.js
+++ test/lib/integration.js
@@ -18,131 +18,82 @@ module.exports = () => {
   fsExtra.ensureDirSync(projectsBuildPath);
   fsExtra.ensureSymlinkSync(`${appRoot}/bin/bin.js`, '/usr/bin/sqz');
 
-  const cb = (err, stdout, stderr, cmd, done) => {
-    if (!_.isEmpty(err)) {
-      done(new Error());
-      process.stdout.write(`\nCOMMAND : ${colors.blue.bold(cmd)}\n`);
-      process.stdout.write(colors.red.bold(`\n${stdout}`));
-      process.exit(1);
-    } else {
-      done();
-    }
+  const shtest = (cmd, done, regex) => {
+    const cb = (err, stdout, stderr) => {
+      if (!_.isEmpty(err)) {
+        done(new Error());
+        process.stdout.write(`\nCOMMAND : ${colors.blue.bold(cmd)}\n`);
+        process.stdout.write(colors.red.bold(`\n${stdout || stderr}`));
+        process.exit(1);
+      } else {
+        done();
+      }
+    };
+
+    return shelltest()
+      .cmd(cmd)
+      .expect('stdout', regex)
+      .expect(0)
+      .end((err, stdout, stderr) => cb(err, stdout, stderr));
   };
 
+  const testTemplates = (cmd, regex) => {
+    _.forEach(templates, (template) => {
+      const templateCmd = cmd.replace(/#template/gi, template);
+      it(templateCmd, (done) => {
+        shtest(templateCmd, done, regex);
+      });
+    });
+  };
+
+  it('should run the "hwclock" command', (done) => {
+    shtest('hwclock -s', done, /.*/);
+  });
+
   it('should run the "list" command', (done) => {
-    const cmd = `${baseCommand} list`;
-    shelltest()
-      .cmd(cmd)
-      .expect('stdout', /Available commands/)
-      .expect(0)
-      .end((err, stdout, stderr) => cb(err, stdout, stderr, cmd, done));
+    shtest(`${baseCommand} list`, done, /Available commands/);
   });
 
   it('should run the "help" command', (done) => {
-    const cmd = `${baseCommand} help config`;
-    shelltest()
-      .cmd(cmd)
-      .expect('stdout', /Usage/)
-      .expect(0)
-      .end((err, stdout, stderr) => cb(err, stdout, stderr, cmd, done));
+    shtest(`${baseCommand} help config`, done, /Usage/);
   });
 
   it('should run the "version" command', (done) => {
-    const cmd = `${baseCommand} version`;
-    shelltest()
-      .cmd(cmd)
-      .expect('stdout', /CLI version/)
-      .expect(0)
-      .end(done);
+    shtest(`${baseCommand} version`, done, /CLI version/);
   });
 
   parallel('create projects based on all available templates', () => {
-    _.forEach(templates, (template) => {
-      const cmd = `cd ${projectsBuildPath} && ${baseCommand} create --project ${template} --template ${template} --email test@squeezer.io --noChecksums true`;
-      it(`creating test project ${template}\n     Running command : ${cmd}`, (done) => {
-        shelltest()
-          .cmd(cmd)
-          .expect('stdout', /Project successfully created/)
-          .expect(0)
-          .end((err, stdout, stderr) => cb(err, stdout, stderr, cmd, done));
-      });
-    });
+    const cmd = `cd ${projectsBuildPath} && ${baseCommand} create --project #template --template #template --email test@squeezer.io --noChecksums true`;
+    testTemplates(cmd, /Project successfully created/);
   });
 
-  describe('configuring credentials', () => {
-    _.forEach(templates, (template) => {
-      const cmd = `cd ${projectsBuildPath}/${template} && ${baseCommand} config --setting aws_profile --value ${awsTestProfile}`;
-      it(`configuring AWS profile on project ${template}\n     Running command : ${cmd}`, (done) => {
-        shelltest()
-          .cmd(cmd)
-          .expect('stdout', /Done !/)
-          .expect(0)
-          .end((err, stdout, stderr) => cb(err, stdout, stderr, cmd, done));
-      });
-    });
+  describe('configuring clouds credentials', () => {
+    const cmd = `cd ${projectsBuildPath}/#template && ${baseCommand} config --setting aws_profile --value ${awsTestProfile}`;
+    testTemplates(cmd, /Done !/);
   });
 
   parallel('installing packages on all projects', () => {
-    _.forEach(templates, (template) => {
-      const cmd = `cd ${projectsBuildPath}/${template} && ${baseCommand} install`;
-      it(`installing packages for project ${template}\n     Running command : ${cmd}`, (done) => {
-        shelltest()
-          .cmd(cmd)
-          .expect('stdout', /Installed !/)
-          .expect(0)
-          .end((err, stdout, stderr) => cb(err, stdout, stderr, cmd, done));
-      });
-    });
+    const cmd = `cd ${projectsBuildPath}/#template && ${baseCommand} install`;
+    testTemplates(cmd, /Installed !/);
   });
 
-  parallel('development compile all projects', () => {
-    _.forEach(templates, (template) => {
-      const cmd = `cd ${projectsBuildPath}/${template} && ${baseCommand} compile`;
-      it(`compiling project ${template}\n     Running command : ${cmd}`, (done) => {
-        shelltest()
-          .cmd(cmd)
-          .expect('stdout', /Compiled !/)
-          .expect(0)
-          .end((err, stdout, stderr) => cb(err, stdout, stderr, cmd, done));
-      });
-    });
+  parallel('development compile current projects', () => {
+    const cmd = `cd ${projectsBuildPath}/#template && ${baseCommand} compile`;
+    testTemplates(cmd, /Compiled !/);
   });
 
   parallel('production compile all projects', () => {
-    _.forEach(templates, (template) => {
-      const cmd = `cd ${projectsBuildPath}/${template} && ${baseCommand} compile --production`;
-      it(`compiling project ${template}\n     Running command : ${cmd}`, (done) => {
-        shelltest()
-          .cmd(cmd)
-          .expect('stdout', /Compiled !/)
-          .expect(0)
-          .end(done);
-      });
-    });
+    const cmd = `cd ${projectsBuildPath}/#template && ${baseCommand} compile --production`;
+    testTemplates(cmd, /Compiled !/);
   });
 
   parallel('deploying all projects', () => {
-    _.forEach(templates, (template) => {
-      const cmd = `cd ${projectsBuildPath}/${template} && ${baseCommand} deploy`;
-      it(`deploying project ${template}\n     Running command : ${cmd}`, (done) => {
-        shelltest()
-          .cmd(cmd)
-          .expect('stdout', /Saving checksums/)
-          .expect(0)
-          .end((err, stdout, stderr) => cb(err, stdout, stderr, cmd, done));
-      });
-    });
+    const cmd = `cd ${projectsBuildPath}/#template && ${baseCommand} deploy`;
+    testTemplates(cmd, /Saving checksums/);
   });
 
   parallel('testing all projects', () => {
-    _.forEach(templates, (template) => {
-      const cmd = `sh -c 'cd ${projectsBuildPath}/${template} && ${baseCommand} test'`;
-      it(`testing project ${template}\n     Running command : ${cmd}`, (done) => {
-        shelltest()
-          .cmd(cmd)
-          .expect(0)
-          .end((err, stdout, stderr) => cb(err, stdout, stderr, cmd, done));
-      });
-    });
+    const cmd = `sh -c 'cd ${projectsBuildPath}/#template && ${baseCommand} test'`;
+    testTemplates(cmd, /.*/);
   });
 };
